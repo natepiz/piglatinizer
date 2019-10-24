7933e80a431f5ef2093b5ffc9a48d1ec6cee9b68
natepiz <natepizarro140@gmail.com>
2019-10-24 21:55:57 +0000

    this is a message :)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+//this helps your jQuery to work. Write all of your JS code in this function.
+
+$( "document" ).ready(function() {
 
 
 
-  
-  
-  
-  
+  $("button").click(
+      function() {
+       let word =   $("input").val();
+       $(".output").text(word + "ay");
+       let vowls = ["a","e","i","o","u","and sometimes y"]
+      });
+
+
+
 });
