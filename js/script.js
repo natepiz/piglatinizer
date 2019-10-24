fdf24783fbda00c02d0886c7d7c98acd38da75d8
natepiz <natepizarro140@gmail.com>
2019-10-24 22:00:05 +0000

    wip

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ $( "document" ).ready(function() {
       function() {
        let word =   $("input").val();
        $(".output").text(word + "ay");
-       let vowls = ["a","e","i","o","u","and sometimes y"]
+       let vowls = ["a","e","i","o","u","and sometimes y"];
+    //hey nate
       });
 
 
